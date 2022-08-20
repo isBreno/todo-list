8f69a3efc8d50ab8170a5632a2e78daca6597c49
Breno <eusoubrenovieira@gmail.com>
2022-08-20 11:55:33 -0300

    added notasks interface

--- src/pages/Home/components/Board/index.tsx
+++ src/pages/Home/components/Board/index.tsx
@@ -13,13 +13,15 @@ export const Board = () => {
     window.location.reload()
   }
   // get tasks from localStorage array
-  const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+  const tasks = JSON.parse(localStorage.getItem("tasks")!) || "";
 
   return (
     <BoardContainer>
-      <button onClick={handleClearTasks}>Limpar tudo</button>
       {tasks ? (
-        tasks.map((task: any) => <Tasks task={task} key={task.id} />)
+        <>
+          <button onClick={handleClearTasks}>Limpar tudo</button>
+          {tasks.map((task: any) => <Tasks task={task} key={task.id} />)}
+        </>
       ) : (
         <NoTasks />
       )}
